e8157950cfbc69b9a95a47a6e4c1ecf8be03ee9d
Matias <matiasandinoma@gmail.com>
2023-11-24 07:58:12 -0300

    avances banner

--- src/components/banner/Contenido.jsx
+++ src/components/banner/Contenido.jsx
@@ -3,31 +3,23 @@ import Carrusel from '../carrusel/Carrusel'
 import Card from '../card/Card'
 
 const Contenido = ({ parent }) => {
-    const sobre = {
-        // display: 'flex',
-        // alignSelf: 'flex-start',
-        position: 'absolute',
-        top: '15%',
-        zIndex:'1',
 
-    }
+    /*
+    * se modifico el ancho del div padre de card w-75
+    * se le dio a card w-50
 
+
+
+    */
     return (
-        <div style={sobre} >
-
-            <div className="row">
+        <div>
+            <div className="position-relative mb-2">
                 {
                     parent === 'MainView'
                         ? (
-                            <div className=''>
-                                {/* <div className="col-12 row align-items-end" style={{ width:'150vw', height: '100%' }}> */}
-                                    {/* <div className="col-5"> */}
-                                        <Card titulo='The Office' introduccion='El iluso jefe Michael intenta encauzar el descontento de sus empleados en la empresa papelera Dunder Mifflin a traves de continuas debilidades y percances.' />
-                                    {/* </div> */}
-                                {/* </div> */}
-                                {/* <div className="col-12"> */}
-                                    {/* <Carrusel /> */}
-                                {/* </div> */}
+                            <div className='w-100 position-absolute bottom-0'>
+                                <Card titulo='The Office' introduccion='El iluso jefe Michael intenta encauzar el descontento de sus empleados en la empresa papelera Dunder Mifflin a traves de continuas debilidades y percances.' />
+                                <Carrusel />
                             </div>
                         )
                         :
--- src/components/banner/background-image/BackgroundImage.jsx
+++ src/components/banner/background-image/BackgroundImage.jsx
@@ -10,8 +10,8 @@ const BackgroundImage = () => {
 
   return (
     <div>
-      <img src='src/assets/imagenes/16x9-solid.jpg' className="" alt="..." style={ style }/>
-      {/* <img src='https://occ-0-3791-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABU1Czziam0slhA3T09BuJd70yj6v2tt42thd3cvpM-2E_bOAfttQ-b2wITR4RDJKkACCSUSnK7V20Fp7K7q5CBUsEG9MaTwgXQFq.webp?r=456' className="" alt="..." style={ style }/> */}
+      {/* <img src='src/assets/imagenes/16x9-solid.jpg' className="" alt="..." style={ style }/> */}
+      <img src='https://occ-0-3791-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABU1Czziam0slhA3T09BuJd70yj6v2tt42thd3cvpM-2E_bOAfttQ-b2wITR4RDJKkACCSUSnK7V20Fp7K7q5CBUsEG9MaTwgXQFq.webp?r=456' className="" alt="..." style={ style }/>
     </div>
   )
 }
