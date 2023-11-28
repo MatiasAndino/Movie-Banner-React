be55034296ae0b6310fc4f1eaa2b87e4369cf499
Matias <matiasandinoma@gmail.com>
2023-11-28 10:31:49 -0300

    terminado gradient entre la imagen y el color de fondo

--- src/components/banner/background-image/BackgroundImage.jsx
+++ src/components/banner/background-image/BackgroundImage.jsx
@@ -9,11 +9,12 @@ const BackgroundImage = () => {
   };
 
   return (
-    <div>
+    <div className='ban position-relative' style={{height: '100%'}}>
       {/* <img src='src/assets/imagenes/16x9-solid.jpg' className="" alt="..." style={ style }/> */}
       <img src='https://occ-0-3791-185.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABU1Czziam0slhA3T09BuJd70yj6v2tt42thd3cvpM-2E_bOAfttQ-b2wITR4RDJKkACCSUSnK7V20Fp7K7q5CBUsEG9MaTwgXQFq.webp?r=456' className="" alt="..." style={ style }/>
+      <div className='gradient position-absolute top-50'></div>
     </div>
   )
 }
 
-export default BackgroundImage
+export default BackgroundImage;
